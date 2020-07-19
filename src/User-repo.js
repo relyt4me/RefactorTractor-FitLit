cdd4cdc726a25134cec2d6bddb1ac8dda8369372
Carly Clift <carlymclift@gmail.com>
2020-07-19 16:09:48 -0600

    Add comments for what the methods are doing

--- src/User-repo.js
+++ src/User-repo.js
@@ -1,63 +1,82 @@
 class UserRepo {
   constructor(users) {
     this.users = users;
-  };
+  }
 
-/* User Repo should have two methods:
-  - get user data for a given user ID
-  - get average step goal for all users
-*/ 
+  /* User Repo should have two methods:
+  - get user data for a given user ID- getDataFromID()
+  - get average step goal for all users- calculateAverageStepGoal()
+  */ 
+
+  //This method taked in a id and returns the user object that matches the id
   getDataFromID(id) {
     return this.users.find((user) => id === user.id);
-  };
+  }
+
+  //Used by activity, hyradtion, and sleep and scripts
+  // it grabs all of the user data from given data set and given an ID 
   getDataFromUserID(id, dataSet) {
     return dataSet.filter((userData) => id === userData.userID);
-  };
+  }
 
-  //This method was passing the test, changed it to pass
+  //This method wasn't passing the test, changed it to pass using Math.round()
   calculateAverageStepGoal() {
-    var totalStepGoal = this.users.reduce((sumSoFar, data) => {
-      return sumSoFar = sumSoFar + data.dailyStepGoal;
+    let totalStepGoal = this.users.reduce((sum, data) => {
+      sum += data.dailyStepGoal
+      return sum
     }, 0);
-    return totalStepGoal / this.users.length;
-  };
+    return Math.round(totalStepGoal / this.users.length)
+  }
 
-//   fetchAverageStepGoal() {
-//     const average = this.data.reduce((acc, user) => {
-//         acc += user.dailyStepGoal
-//         return acc;
-//     }, 0)
-//     return Math.round(average / this.data.length)
-//   }
-// }
+  /* 
+  Methods below shouldn't really be in the UserRepo, 
+  they are better off in scripts or other files
+  however they used everywhere and aren't easy to move
+  */
 
-
-
+  //given a user id and data set this sorts the data by date
+  // sorts most recent to least recent
   makeSortedUserArray(id, dataSet) {
     let selectedID = this.getDataFromUserID(id, dataSet)
     let sortedByDate = selectedID.sort((a, b) => new Date(b.date) - new Date(a.date));
     return sortedByDate;
   }
+
+  /* 
+  method doesn't seem very useful since you can just invoke the 
+  makeSortedUserArray and get the date of first index
+  */
   getToday(id, dataSet) {
     return this.makeSortedUserArray(id, dataSet)[0].date;
-  };
+  }
+
+  // This returns the EXACT same thing as makeSortedUserArray()
+  // see lins 85-110 of testing file
   getFirstWeek(id, dataSet) {
     return this.makeSortedUserArray(id, dataSet).slice(0, 7);
-  };
+  }
+
+  // returns back the same thing you pass in, see test
+  // not sure the use of this 
   getWeekFromDate(date, id, dataSet) {
     let dateIndex = this.makeSortedUserArray(id, dataSet).indexOf(this.makeSortedUserArray(id, dataSet).find((sortedItem) => (sortedItem.date === date)));
     return this.makeSortedUserArray(id, dataSet).slice(dateIndex, dateIndex + 7);
-  };
+  }
+
+  // returns data of whatever date and data set you pass in, for all users
   chooseWeekDataForAllUsers(dataSet, date) {
     return dataSet.filter(function(dataItem) {
       return (new Date(date)).setDate((new Date(date)).getDate() - 7) <= new Date(dataItem.date) && new Date(dataItem.date) <= new Date(date)
     })
-  };
+  }
+
   chooseDayDataForAllUsers(dataSet, date) {
     return dataSet.filter(function(dataItem) {
       return dataItem.date === date
     });
   }
+
+  // Has parameters that are not used?
   isolateUsernameAndRelevantData(dataSet, date, relevantData, listFromMethod) {
     return listFromMethod.reduce(function(objectSoFar, dataItem) {
       if (!objectSoFar[dataItem.userID]) {
@@ -66,8 +85,9 @@ class UserRepo {
         objectSoFar[dataItem.userID].push(dataItem[relevantData])
       }
       return objectSoFar;
-    }, {});
+    }, {})
   }
+
   rankUserIDsbyRelevantDataValue(dataSet, date, relevantData, listFromMethod) {
     let sortedObjectKeys = this.isolateUsernameAndRelevantData(dataSet, date, relevantData, listFromMethod)
     return Object.keys(sortedObjectKeys).sort(function(b, a) {
@@ -78,8 +98,10 @@ class UserRepo {
         sumSoFar += sleepQualityValue
         return sumSoFar;
       }, 0) / sortedObjectKeys[b].length)
-    });
+    })
   }
+
+  // This method is putting out a long float, needs to be rounded
   combineRankedUserIDsAndAveragedData(dataSet, date, relevantData, listFromMethod) {
     let sortedObjectKeys = this.isolateUsernameAndRelevantData(dataSet, date, relevantData, listFromMethod)
     let rankedUsersAndAverages = this.rankUserIDsbyRelevantDataValue(dataSet, date, relevantData, listFromMethod)
@@ -90,9 +112,9 @@ class UserRepo {
             sumSoFar += sleepQualityValue
             return sumSoFar;
           }, 0) / sortedObjectKeys[rankedUser].length
-      };
+      }
       return rankedUser;
-    });
+    })
   }
 }
 
