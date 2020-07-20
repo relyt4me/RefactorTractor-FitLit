59bf58823ebdc5395113eb34d437daf5cbc37bb5
Carly Clift <carlymclift@gmail.com>
2020-07-20 17:33:46 -0600

    Fix last test to use the correct data sample set

--- test/user-test.js
+++ test/user-test.js
@@ -17,7 +17,6 @@ describe('User', () => {
     userRepo = new UserRepo(users);
   });
 
-
   it('Should be a function', () => {
     expect(User).to.be.a('function');
   });
@@ -33,7 +32,7 @@ describe('User', () => {
     expect(user1.email).to.equal('Diana.Hayes1@hotmail.com');
     expect(user1.strideLength).to.equal(4.3);
     expect(user1.dailyStepGoal).to.equal(10000);
-    expect(user1.friends).to.deep.equal([2, 4, 8]);
+    expect(user1.friends).to.deep.equal([2, 4, 3]);
   });
 
   it('Should return user first name', () => {
@@ -41,45 +40,6 @@ describe('User', () => {
   });
 
   it('Should return list of friend names from user repository', () => {
-    user1 = new User({
-      id: 1,
-      name: "Alex Roth",
-      address: "1234 Turing Street, Denver CO 80301-1697",
-      email: "alex.roth1@hotmail.com",
-      strideLength: 4.3,
-      dailyStepGoal: 10000,
-      friends: [2, 3, 4]
-    });
-    user2 = new User({
-      id: 2,
-      name: "Allie McCarthy",
-      address: "1235 Turing Street, Denver CO 80301-1697",
-      email: "allie.mcc1@hotmail.com",
-      strideLength: 3.3,
-      dailyStepGoal: 9000,
-      friends: [1, 3, 4]
-    });
-
-    user3 = new User({
-      id: 3,
-      name: "The Rock",
-      address: "1236 Awesome Street, Denver CO 80301-1697",
-      email: "therock@hotmail.com",
-      strideLength: 10,
-      dailyStepGoal: 60000,
-      friends: [1, 2, 4]
-    });
-
-    user4 = new User({
-      id: 4,
-      name: "Rainbow Dash",
-      address: "1237 Equestria Street, Denver CO 80301-1697",
-      email: "rainbowD1@hotmail.com",
-      strideLength: 3.8,
-      dailyStepGoal: 7000,
-      friends: [1, 2, 3]
-    });
-  
-    expect(user2.getFriendsNames(userRepo)).to.deep.equal(['Luisa Hane', 'Herminia Witting', 'Mae Connelly']);
+    expect(user1.getFriendsNames(userRepo)).to.deep.equal(['Jarvis Considine', 'Mae Connelly', 'Herminia Witting']);
   });
 });
