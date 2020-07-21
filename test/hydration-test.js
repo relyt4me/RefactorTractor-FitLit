fbc5a98abc01639972319d8676e666804e043df1
Tyler Haglund <relyt4me@gmail.com>
2020-07-21 15:45:45 -0600

    Rewrite single method for getWeekOfOunces in new Format

--- test/hydration-test.js
+++ test/hydration-test.js
@@ -135,13 +135,35 @@ describe.only('Hydration', function () {
     expect(noHydrationData.hydrationData).to.eql(undefined);
   });
 
-  it('should find the average water intake per day for a user', function () {
+  it('Should find the average water intake per day for a user', function () {
     expect(hydrationRepo.calculateAverageOunces(3)).to.equal(55.86);
   });
 
-  it('should find the water intake for a user on a specified date', function () {
-    expect(hydration.calculateDailyOunces(1, '2019/06/15')).to.equal(37);
-    expect(hydration.calculateDailyOunces(4, '2019/04/15')).to.equal(36);
+  it('Should return undefined when calculateAverage is given for a nonExistant user', () => {
+    expect(hydrationRepo.calculateAverageOunces(6)).to.eql(undefined);
+  });
+
+  it('Should find the water intake for a user on a specified date', function () {
+    expect(hydrationRepo.calculateDailyOunces(1, '2019/06/15')).to.equal(37);
+    expect(hydrationRepo.calculateDailyOunces(4, '2019/06/18')).to.equal(93);
+  });
+
+  it('Should return undefined if the date or id does not exist', function () {
+    expect(hydrationRepo.calculateDailyOunces(6, '2019/06/15')).to.equal(undefined);
+    expect(hydrationRepo.calculateDailyOunces(4, '2020/02/31')).to.equal(undefined);
+  });
+
+  it('Should find the water intake for a user for one week based on a final date', () => {
+    const weekOfOuncesForUser2 = [
+      { date: '2019/06/15', amount: 38, unit: 'oz' },
+      { date: '2019/06/16', amount: 91, unit: 'oz' },
+      { date: '2019/06/17', amount: 96, unit: 'oz' },
+      { date: '2019/06/18', amount: 70, unit: 'oz' },
+      { date: '2019/06/19', amount: 76, unit: 'oz' },
+      { date: '2019/06/20', amount: 71, unit: 'oz' },
+      { date: '2019/06/21', amount: 27, unit: 'oz' },
+    ];
+    expect(hydrationRepo.getWeekOfOunces(2, '2019/06/21')).to.deep.eql(weekOfOuncesForUser2);
   });
 
   it('should find water intake by day for first week', function () {
