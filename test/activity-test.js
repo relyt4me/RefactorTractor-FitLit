cd84e0a6a595e18228ea94fc062671dbe3c0ae03
Jordy Shryock <jordy1611@gmail.com>
2020-07-21 17:13:02 -0600

    Update tests to take in updated parameters

--- test/activity-test.js
+++ test/activity-test.js
@@ -296,7 +296,7 @@ describe('Activity', function() {
   it('should return minutes active for given user on given date', function() {
     expect(activity.userDataForToday(1, "2019/06/18", userRepo, 'minutesActive')).to.eql(62);
   });
-  it('should return a weeks worth steps for a given user', function() {
+  it('should return a weeks worth of steps for a given user', function() {
     expect(activity.userDataForWeek(1, "2019/06/23", userRepo, 'numSteps')[0]).to.eql("2019/06/23: 9000");
     expect(activity.userDataForWeek(1, "2019/06/23", userRepo, 'numSteps')[3]).to.eql("2019/06/20: 9303");
   });
